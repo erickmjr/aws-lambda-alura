266c7f45bd88344ddd6ed1a3f32ddf3f84b060af
bgc-erick-mello <erick.mello@bgcbrasil.com.br>
2026-03-15 12:44:41 -0300

    fix: object key

--- index.mjs
+++ index.mjs
@@ -1,13 +1,13 @@
 import { log } from "./log.mjs";
 import { S3 } from '@aws-sdk/client-s3'; 
 
-const s3Client = new S3({ region: 'us-east-1' });
+const s3Client = new S3({ region: 'sa-east-1' });
 
 export const handler = async (event) => {
 
     const record = event.Records[0];
     const Bucket = record.s3.bucket.name;
-    const Key = record.s3.bucket.key;
+    const Key = record.s3.object.key;
     const getObjectResult = await s3Client.getObject({
         Bucket,
         Key
